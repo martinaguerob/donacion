7129dec069be3e906602595157a05cd66cd732cf
Martin Aguero <martinaguerobarreto@gmail.com>
2020-10-27 01:30:25 -0500

    donacion spring

---
+++ src/test/java/pe/edu/upc/donacion/DonacionApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.donacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
